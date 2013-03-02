c3d7d0febfb683f6ca23bb58a17d8d4adb733d43
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2013-03-03 00:49:38 +0900

    Added the main mlib.py file with the `entry point'

---
+++ mlib.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+'''
+Created on 27 Feb 2013
+
+@author: 7k
+'''
+import os
+import re
+import sys
+
+DEFAULTS = {
+    'movies': 'Movies',
+    'music': 'Music',
+}
+
+RE_MOVIES = re.compile('.*\.(mov|MOV|avi|AVI|mpg|MPG|mp4|MP4|mkv|MKV)$')
+
+
+def get_movies(searchdir):
+    matches = []
+    for root, _dirnames, filenames in os.walk(searchdir):
+        for filename in filter(lambda n: RE_MOVIES.match(n), filenames):
+            matches.append(os.path.join(root, filename))
+    return matches
+
+
+class Library:
+    def __init__(self, path, movies=DEFAULTS['music'], music=DEFAULTS['music']):
+        self.path = path
+        self.movies = movies
+        self.music = music
+
+    @property
+    def movies_path(self):
+        return os.path.join(self.path, self.movies)
+
+    def path_for_tv_season(self, show, season):
+        return os.path.join(self.movies_path, show, 'Season %s' % season)
+
+
+if __name__ == '__main__':
+    from commands import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
